1140cec975d95949dd5b760de3e8797889e1a582
Duc Anh Phan <ducanh.101296@gmail.com>
2024-03-14 21:28:43 +1100

    update

--- js/main.js
+++ js/main.js
@@ -30,3 +30,78 @@ function tinhTong() {
 getEle("tinhTong").onclick = function(){
     tinhTong();
 }
+
+// Bài 3
+function tinhGiaiThua() {
+    var n =+getEle("soNBai3").value ;
+    var tongGiaiThua = 1;
+    for(var i=1; i<n+1;i++) {
+        tongGiaiThua *=i ;
+    }
+    getEle("result3").innerHTML =`Giai thừa của ${n} là ${tongGiaiThua}`;
+}
+
+getEle("tinhGiaiThua").onclick = function() {
+    tinhGiaiThua();
+}
+
+// bài 4 
+function inTheDiv() {
+    var div = document.createElement("div");
+    var result4 = getEle("result4");
+
+    for(var i=1; i<11;i++) {
+        if (i%2===0){
+            var div = document.createElement("div");
+            div.classList.add("even-div");
+            result4.appendChild(div);
+
+        } else {
+            var div = document.createElement("div");
+            div.classList.add("odd-div");
+            result4.appendChild(div);
+
+        }
+    }
+
+
+}
+
+getEle("inTheDiv").onclick = function() {
+    inTheDiv()
+}
+
+// Bài 5
+function isPrime(num) {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+    if (num % 2 === 0 || num % 3 === 0) return false;
+
+    for (let i = 2; i * i <= num; i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+
+function timSoNguyenTo() {
+    var soNguyen = +getEle("soNguyen").value;
+    var soNguyenTo = "";
+    var note = "";
+
+    if (soNguyen < 1) {
+        note = "Không có số nguyên tố nào";
+    } else {
+        for (var i = 1; i <= soNguyen; i++) {
+            if (isPrime(i)) {
+                soNguyenTo += `${i} `;
+            }
+        }
+        note = `Các số nguyên tố từ 1 tới ${soNguyen} là: ${soNguyenTo}`;
+    }
+
+    getEle("result5").innerHTML = note;
+}
+
+getEle("timSoNguyenTo").onclick = function () {
+    timSoNguyenTo();
+}
